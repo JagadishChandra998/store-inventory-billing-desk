0f75c06115b4b1c86b67165b3917cd105f9bcc16
Jagadish <jagadishchandra998@gmail.com>
2026-08-21 11:34:44 +0530

    Show login error

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -131,7 +131,7 @@ export const loginUser = async (req, res) => {
     }
 
     catch (error) {
-        console.error(error);
+        console.error("login error",error);
         res.status(500).json({
             success: false,
             message: "Server error",
